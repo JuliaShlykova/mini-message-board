609ba8c8f00c4692d602e57f8dd2c4170c56442b
Julia Shlykova <julia.shlykova17@gmail.com>
2023-03-17 19:16:06 +0300

    limiting requests

--- app.js
+++ app.js
@@ -3,15 +3,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+//limit requests
+const limiter = rateLimit({
+  windowMs: 1*60*1000,//1 minute
+  max:20//20 requests per each IP
+})
+app.use(limiter)
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,42 @@
 var express = require('express');
 var router = express.Router();
+const {DateTime} = require('luxon');
+const {body, validationResult} = require('express-validator');
+
+const formattedDate = (d) => {
+  return DateTime.fromJSDate(d).toLocaleString(DateTime.DATE_MED)
+}
+
+const Message = (text, user) => {
+  const added = formattedDate(new Date());
+  return {text, user, added};
+}
+
+let messages = [Message("Hi there!", "Amando"), Message("Hello World!","Charles")]
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini messageboard', messages });
 });
+
+router.get('/new', function(req,res,next) {
+  res.render('form');
+});
+
+router.post('/new',
+  body('message', 'Write something!').isLength({min:1}).escape(),
+  body('user', 'Name yourself!').trim().isLength({min:1}).escape(),
+  function(req, res, next) {
+    const msg = Message(req.body.message, req.body.user);
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+      res.render('form', {
+        msg,
+        errors: errors.array()
+      })
+    }
+    messages.push(msg);
+    res.redirect('/');
+})
 
 module.exports = router;
